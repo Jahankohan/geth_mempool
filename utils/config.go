6d0f8409de08c8f298db0e86f641febdfbbc0956
Reza Jahankohan <r.jahankohan@gmail.com>
2022-05-08 14:22:07 +0430

    just updated config file with new variables

--- utils/config.go
+++ utils/config.go
@@ -2,4 +2,7 @@ package utils
 
 type Config struct {
 	DebugMode	bool	`mapstructure:"DEBUG_MODE"`
+	DefaultNetwork	string	`mapstructure:"DEFAULT_NETWORK"`
+	LocalNetwork string		`mapstructure:"LOCAL_NET"`
+	RemoteNetwork string		`mapstructure:"REMOTE_NET"`
 }
--- utils/load_config.go
+++ utils/load_config.go
@@ -1,32 +1,30 @@
 package utils
 
 import (
+	"log"
+
 	"github.com/spf13/viper"
 )
 
-func LoadConfig(path string) (config Config, err error) {
-	viper.AddConfigPath(path)
+var variable *Config
 
+func InitConfig() {
+	viper.AddConfigPath(".")
 	viper.SetConfigName(".env")
-
 	viper.SetConfigType("env")
-
 	viper.AutomaticEnv()
 
-	err = viper.ReadInConfig()
+	err := viper.ReadInConfig()
 	if err != nil {
-		return
+		log.Fatal(err)
 	}
 
-	err = viper.Unmarshal(&config)
-	return
+	err = viper.Unmarshal(&variable)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
-func InitConfig() (Config, error) {
-	config, err := LoadConfig(".")
-	if err != nil {
-		return Config{}, err
-	}
-
-	return config, nil
+func GetConfig() (*Config) {
+	return variable
 }
